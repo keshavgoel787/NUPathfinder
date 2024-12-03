30c347d046a306a215fdaab3636ec259c4024226
Tommaso123456 <64472792+Tommaso123456@users.noreply.github.com>
2024-12-03 10:12:21 -0500

    Rework of Listings

--- app/src/pages/Listings.py
+++ app/src/pages/Listings.py
@@ -20,25 +20,46 @@ url = f"http://api:4000/r/Listings/{st.session_state['rec_id']}"
 
 listings = requests.get(url).json()
 
-posArray = []
-descArray = []
-startDates = []
-endDates = []
 
-for i in listings:
-    posArray.append(i['position'])
-    descArray.append(i['description'])
-    startDates.append(i['startDate'][6:17])
-    endDates.append(i['endDate'][6:17])
 
-df = pd.DataFrame(
-    {
-        "Position": posArray,
-        "Description": descArray,
-        "Start date": startDates,
-        "End date": endDates,
-    }
-)
+if listings:
+    for i in listings:
+        st.write(f"Position: {i['position']}")
+        st.write(f"Description: {i['description']}")
+        st.write(f"Start date: {i['startDate'][6:17]}")
+        st.write(f"end date: {i['endDate'][6:17]}")
+        if st.button("View applicants", key=f"view{i['jobId']}"):
+            st.session_state['applicant_job_id'] = i['jobId']
+            st.session_state['applicant_job_name'] = i['position']
+            st.switch_page('pages/viewApplicants.py')
+
+        st.write("---")
+    else:
+         if st.button("Add another one"):
+            st.session_state['recState'] = 'adding'
+            st.switch_page('pages/AddListings.py')
+
+else:
+    st.write('You do not currently have any listings')
+    if st.button("Add one now!"):
+        st.session_state['recState'] = 'adding'
+        st.switch_page('pages/AddListings.py')
+
+
+
+# df = pd.DataFrame(
+#     {
+#         "Position": posArray,
+#         "Description": descArray,
+#         "Start date": startDates,
+#         "End date": endDates,
+#     }
+# )
    
+# st.dataframe(df, hide_index = True)
 
-st.dataframe(df, hide_index = True)
+        
+
+
+
+
