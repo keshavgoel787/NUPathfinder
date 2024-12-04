7bf67065b2f77e5e806f5dd32c86d9e161a4fbe5
Keshav Goel <141261924+keshavgoel787@users.noreply.github.com>
2024-12-04 00:23:53 -0500

    Update text

--- app/src/pages/CourseSkill.py
+++ app/src/pages/CourseSkill.py
@@ -12,7 +12,7 @@ st.set_page_config(layout = 'wide')
 # Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
-st.title(f"Update a skill for {st.session_state['Current Course']}")
+st.title(f"Update skill for {st.session_state['Current Course']}")
 
 courseid = st.session_state['Current Course #']
 department_id = st.session_state.get('department_ID', 1)
